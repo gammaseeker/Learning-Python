779b4c3f927872e6b620141b7da89270a5e35e21
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-18 22:19:12 -0400

    Add files via upload

---
+++ PyPet/PyPet.py
@@ -0,0 +1,19 @@
+print ("Welcome to PyPet!")
+
+name = "Pepper"
+age = 5
+weight = 9.5
+hungry = False
+photo = "(=^o.o^=)__"
+
+cat = {
+	"name": "Pepper",
+	"hungry": True,
+	"weight": 9.5,
+	"age": 5,
+	"photo": "(=^o.o^=)__",
+}
+
+print (cat)
+print ("Hello " + name)
+print (photo) 
