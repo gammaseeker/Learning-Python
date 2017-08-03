8b698eb463163b30592daa1c1e1efc32001c09ad
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-03 09:59:04 -0400

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,6 +1,22 @@
 from sklearn.datasets import fetch_mldata
+%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 mnist = fetch_mldata('MNIST original')
 print(mnist)
 X, y = mnist["data"], mnist["target"]
 print(X.shape)
 print(y.shape)
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap = matplotlib.cm.binary,
+           interpolation="nearest")
+plt.axis("off")
+plt.show()
+print(y[36000])
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+shuffle_index = np.random.permutation(60000)
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
