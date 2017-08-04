1ecba70249864402cf79d1e48f16ef28d564d8b0
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-04 17:31:35 -0400

    added tuples and dictionaries

--- Lessons/data_types.py
+++ Lessons/data_types.py
@@ -23,3 +23,21 @@ print list[1:3] #Prints elements from index 1 to index 2
 print list[2:] #Prints elements from index 2 to end
 print tinylist * 2 #Prints list two times
 print list + tinylist #concat both lists
+
+#tuples
+tuple = ( 'abcd', 786, 2.23, 'john', 70.2 )
+list = [ 'abcd', 786, 2.23, 'john', 70.2 ]
+# tuple[2] = 1000 #Invalid syntax with tuple
+list[2] = 1000 #Vaid with list
+
+#dictionary
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+dict2 = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+
+print dict['one'] #Prints value for 'one' key
+print dict[2] # Prints value for 2 key
+print dict2 # Prints complete dictionary
+print dict2.keys() # Prints all the keys
+print dict2.values() # Prints all the values
