446f7ac332b79683dbd95698b8f0a3cf808eaeb6
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-04 17:20:36 -0400

    data types

---
+++ Lessons/data_types.py
@@ -0,0 +1,25 @@
+#Python data types
+
+#numbers
+var1 = 1
+var2 = 10
+#supports int, long, float, complex
+
+#strings
+str = 'Hello World!'
+print str #Prints complete string
+print str[0] #Prints first character of string
+print str[2:5] #Prints characters from index 2 to index 4
+print str[2:] #Prints string from index 2 to end
+print str * 2 # Prints string two times
+print str + "TEST" #Prints concatenated string
+
+#lists
+list = [ 'abd', 786, 2.33, 'john', 70.2 ]
+tinylist = [123, 'john' ]
+print list #Prints complete list
+print list[0] #Prints first element of list
+print list[1:3] #Prints elements from index 1 to index 2
+print list[2:] #Prints elements from index 2 to end
+print tinylist * 2 #Prints list two times
+print list + tinylist #concat both lists
