dc1ec09f2a9c0b98b9da17aaa721f1f568216033
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-04 17:42:09 -0400

    Add files via upload

---
+++ Lessons/numbers.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+print len(str(2 ** 1000000))
+3.1415 * 2 #repr
+print (3.1415 * 2) #str
+print math.pi
+print math.sqrt(85)
+print random.random()
+print random.choice([1, 2, 3, 4])
