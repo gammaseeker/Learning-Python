171ac13880605e75c6421913a279a2a1f859b763
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-12 22:10:49 -0400

    tail recursive factorial

---
+++ Lessons/factorial.py
@@ -0,0 +1,12 @@
+
+def main():
+    num = eval(input("Enter number for factorial: "))
+    print(str(num) + "! =", factorial(num))
+
+def factorial(num):
+    if(num == 0 or num == 1):
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+main()
