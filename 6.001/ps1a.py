4fea33da0b7416e4c8f179236f8908fb98e8b6b5
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-18 18:19:11 -0400

    finished

--- 6.001/ps1a.py
+++ 6.001/ps1a.py
@@ -2,16 +2,22 @@ def main():
     annual_salary = float(input("Enter your annual salary: "))
     portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
     total_cost = float(input("Enter the cost of your dream home:"))
+
+    # Test case
+    # annual_salary = 120000
+    # portion_saved = .1
+    # total_cost = 1000000
     
     portion_down_payment = 0.25
     current_savings = 0.0
     r = 0.04
-    invest = current_savings * r / 12
-    monthly_salary = annual_salary/12
+    invest = current_savings * (r / 12.0)
+    monthly_salary = annual_salary/12.0
     months = 0
 
-    while(current_savings < total_cost * portion_down_payment):
-        current_savings = current_savings + (monthly_salary * portion_saved) + invest
+    while(current_savings < (total_cost * portion_down_payment)):
+        current_savings +=  invest # Monthly interest
+        current_savings += portion_saved * monthly_salary  # Monthly savings
         months = months + 1
 
     print("Enter your annual salary: ", annual_salary)
