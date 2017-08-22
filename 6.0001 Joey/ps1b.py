e3bcf49041000f5ce55555d7490a9c0f06f28ef5
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-21 22:42:34 -0400

    Add files via upload

--- 6.0001 Joey/ps1b.py
+++ 6.0001 Joey/ps1b.py
@@ -1,13 +1,9 @@
 def main():
-    annual_salary = float(input("Enter your annual salary: "))
+    annual_salary = float(input("Enter your annual salary:"))
     portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
     total_cost = float(input("Enter the cost of your dream home:"))
+    semi = float(input("Enter raise:"))
 
-    # Test case
-    # annual_salary = 120000
-    # portion_saved = .1
-    # total_cost = 1000000
-    
     portion_down_payment = 0.25
     current_savings = 0.0
     r = 0.04
@@ -16,9 +12,12 @@ def main():
     months = 0
 
     while(current_savings < (total_cost * portion_down_payment)):
-        current_savings +=  invest # Monthly interest
-        current_savings += portion_saved * monthly_salary  # Monthly savings
         months = months + 1
+        if(months % 6 == 0):
+            monthly_salary= monthly_salary * (1 + semi)
+        current_savings +=  invest
+        current_savings += portion_saved * monthly_salary 
+        
 
     print("Enter your annual salary: ", annual_salary)
     print("Enter the percent of your salary to save, as a decimal: ", portion_saved)
