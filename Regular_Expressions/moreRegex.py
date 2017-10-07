9dad58611fe6fcf5c9f75537812d2ef9772496ec
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-10-06 22:47:46 -0400

    more regex syntax

--- Regular_Expressions/moreRegex.py
+++ Regular_Expressions/moreRegex.py
@@ -16,3 +16,32 @@ phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
 mo = phoneNumRegex.search('My phone number is (415) 555-4242.')
 print(mo.group(1))
 print(mo.group(2))
+
+heroRegex = re.compile (r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey.')
+print(mo1.group())
+mo2 = heroRegex.search('Tina Fey and Batman.')
+print(mo2.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1))
+
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+
+batRegex = re.compile(r'Bat(wo)*man')
+mo3 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo3.group())
+
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batman')
+# print(mo1.group()) Gives you an error because batwoman must appear
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo2.group())
+
