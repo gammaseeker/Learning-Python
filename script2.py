530a57f434a34a1037916119d743b94df1dcb060
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-11-07 17:50:50 -0500

    python script to help with physics hw

---
+++ script2.py
@@ -0,0 +1,30 @@
+"""
+m = 20
+g = 9.81
+h1 = float(input("What is h initial? "))
+h2 = float(input("What is h final? "))
+PE = (m*g*h2) - (m*g*h1)
+print(PE)
+"""
+
+m = 215.90
+v1 = float(input("What is velocity initial? "))
+v2 = float(input("What is velocity final? "))
+KE = (m*v2**2)/2 - (m*v1**2)/2
+print(KE)
+
+"""
+m = 10
+g = 9.81
+h1 = float(input("What is h initial? "))
+h2 = float(input("What is h final? "))
+PE = (m*g*h2) - (m*g*h1)
+print(PE)
+"""
+"""
+m = 10
+v1 = float(input("What is velocity initial? "))
+v2 = float(input("What is velocity final? "))
+KE = (m*v2**2)/2 - (m*v1**2)/2
+print(KE)
+"""
